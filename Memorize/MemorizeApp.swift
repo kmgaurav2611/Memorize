3e8aa25d478a611e6a4726acf6f0d21e277b1f0b
Kumar Gaurav <kmgaurav2611@gmail.com>
2024-06-09 01:18:37 +0530

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Kumar Gaurav on 09/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
